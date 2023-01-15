e1bf507ba4250aa2cf06019896fd2078fca3ef5e
ZeroClian <zeroycl@163.com>
2023-01-15 21:53:15 +0800

    修改配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
         'eslint:recommended'
     ],
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-undef': 'off',
         'vue/no-unused-vars': 'off',
         'vue/require-v-for-key': 'off',
